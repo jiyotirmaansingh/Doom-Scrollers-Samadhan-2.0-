c72fae1b579c3e433c80406f381b66d760129fbf
JIyotirmaan Singh <singhjiyotirmaan@gmail.com>
2025-08-24 11:17:33 +0530

    Add files via upload

---
+++ React Fundamentals/Profile Card/components/Crd.jsx
@@ -0,0 +1,13 @@
+
+function Crd() {
+    return (
+        <div id="card-header">
+            <div id="card-text">
+                <h1>Jiyotirmaan Singh</h1>
+                <p>Full Stack Developer</p>
+                <button id="follow-button">Follow</button>
+            </div>
+        </div>
+    );
+}
+export default Crd;
